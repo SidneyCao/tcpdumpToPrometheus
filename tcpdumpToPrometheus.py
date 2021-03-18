f58c18ab5a67309974caa22bca8bf7bad1627031
Simoncao <13671707129@126.com>
2021-03-18 15:52:53 +0800

    [feat] remove service to reduce metrics significantly

--- tcpdumpToPrometheus.py
+++ tcpdumpToPrometheus.py
@@ -6,9 +6,12 @@ from prometheus_client import Counter, start_http_server
 # Unsupported: ARP, nmap, and multicast
 # Not sure about IPv6 tracking.
 
-metric_labels = ['src', 'dst', 'service', 'proto']
-service_map = {} # Loaded from /etc/services, service_map[port][proto] = service_name
-services = set() # Names of all services
+#metric_labels = ['src', 'dst', 'service', 'proto']
+
+metric_labels = ['src', 'dst']
+
+#service_map = {} # Loaded from /etc/services, service_map[port][proto] = service_name
+#services = set() # Names of all services
 
 # Given an IP or FQDN, extract the domain name to be used as server/client.
 def extract_domain(string):
@@ -36,6 +39,7 @@ pattern = '.*' + '.*'.join([
     ' > ' +
     re_param('dst', '[\w\d\.-]+') + '\.' + re_param('dstp', '[\w\d-]+'),
 ]) + '.*'
+
 dump_matcher = re.compile(pattern)
 
 # Parse output from tcpdump and update the Prometheus counters.
@@ -47,10 +51,12 @@ def parse_packet(line):
 
     labels = {
         'src': extract_domain(m.group('src')),
-        'dst': extract_domain(m.group('dst')),
-        'proto': m.group('proto').lower(),
-        'service': None
+        'dst': extract_domain(m.group('dst'))
+        #'dst': extract_domain(m.group('dst')),
+        #'proto': m.group('proto').lower(),
+        #'service': None
     }
+    '''
     # If the last part of the src/dst is a service, just use the literal service name:
     if m.group('dstp') in services: labels['service'] = m.group('dstp')
     elif m.group('srcp') in services: labels['service'] = m.group('srcp')
@@ -61,6 +67,7 @@ def parse_packet(line):
         labels['service'] = lookup_service(int(m.group('srcp')), labels['proto'])
     if not labels['service']:
         labels['service'] = ""
+    '''
 
     packets.labels(**labels).inc()
     throughput.labels(**labels).inc(int(m.group('length')))
@@ -85,8 +92,9 @@ async def stream_packets():
 
 if __name__ == '__main__':
     # Load a map of ports to services from /etc/services:
+    '''
     matcher = re.compile('(?P<service>\w+)\s*(?P<port>\d+)/(?P<proto>\w+)')
-    with open('/etc/services') as f:
+    with open('./services') as f:
         for line in f.readlines():
             match = matcher.match(line)
             if not match: continue
@@ -94,6 +102,8 @@ if __name__ == '__main__':
             if not port in services: service_map[port] = {}
             service_map[port][match.group('proto')] = match.group('service')
             services.add(match.group('service'))
+    '''
+
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--interface', '-i', default=os.getenv('NTM_INTERFACE', 'eth0'),
@@ -107,9 +117,11 @@ if __name__ == '__main__':
     parser.add_argument('filters', nargs='?', default=os.getenv('NTM_FILTERS', ''),
         help='The TCPdump filters, e.g., "src net 192.168.1.1/24"')
     opts = parser.parse_args()
-
+    
     packets = Counter(f'{opts.metric_prefix}_packets', 'Packets transferred', metric_labels)
     throughput = Counter(f'{opts.metric_prefix}_bytes', 'Bytes transferred', metric_labels)
-
+    
     start_http_server(int(opts.port))
+    
     asyncio.run(stream_packets())
+    
